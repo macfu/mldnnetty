e9f666b91e279e0e1a6ed88bd4e32a4f132f1fbf
liming <v_fuliming@baidu.com>
2018-11-21 17:06:21 +0800

    init httpProgram

---
+++ http-api/src/main/java/com/macfu/commons/ServerInfo.java
@@ -0,0 +1,10 @@
+package com.macfu.commons;
+
+/**
+ * @Author: liming
+ * @Date: 2018/11/21 17:04
+ * @Description: 定义公共的接口
+ */
+public class ServerInfo {
+    public static final int PORT = 80;
+}
